1c1937330ad76d65365ac8226498318371834023
realnifty <rogelioalbertopedraza@gmail.com>
2022-05-16 02:44:11 -0500

    11.4

---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const router = require('express').Router();
+const express = require('express');
+
+router.use(express.static('public'));
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+router.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/animals.html'));
+});
+
+router.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
+});
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+module.exports = router;
