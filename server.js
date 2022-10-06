00129c66c59b2ba80dbc8783d1f27b67db6b7c5d
realnifty <rogelioalbertopedraza@gmail.com>
2022-10-05 20:20:42 -0500

    front end zookeeper functionality

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const htmlRoutes = require("./routes/htmlRoutes");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
-app.use(express.static("public"));
 
 app.listen(PORT, () => {
   console.log("listening on http://localhost:3001");
